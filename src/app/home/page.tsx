99835ce9d71886577d5c72f74d2e82431f87e5f6
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-01-25 15:06:48 +0530

    Crash fixed : (api/route DL)
    Signed-off-by: P SIDHARTH <philkhanasidharth14@gmail.com>

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function page() {
   return (
     <div>
-      <img src="/page.png" alt="" />
+      <Image src="/page.png" alt="page" />
     </div>
   )
 }
